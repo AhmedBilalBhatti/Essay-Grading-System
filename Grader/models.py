06f60362604a397af596c7ec11b5e25b485ea219
Ahmed Bilal Bhatti <110194946+AhmedBilalSSG@users.noreply.github.com>
2024-06-01 21:39:50 +0500

    commit

--- Grader/models.py
+++ Grader/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Registration(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    profile_picture = models.ImageField(upload_to ="media/user/%y")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
