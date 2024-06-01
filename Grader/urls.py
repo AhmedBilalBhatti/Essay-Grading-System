c3211ae3d426ce032bd5cac8057c2e064677d0c1
Ahmed Bilal Bhatti <110194946+AhmedBilalSSG@users.noreply.github.com>
2024-06-01 22:25:32 +0500

    commit

--- Grader/urls.py
+++ Grader/urls.py
@@ -5,4 +5,5 @@ from Grader import *
 urlpatterns = [
     path('home', views.home, name='home'),
     path('', views.register, name='register'),
+    path('login', views.login, name='login'),
 ]
--- Grader/views.py
+++ Grader/views.py
@@ -3,27 +3,50 @@ from django.http import HttpResponse
 from .models import *
 from django.contrib import messages
 
-# Create your views here.
-
-
 
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
+        profile_picture = request.FILES['profile_picture']
 
-        user = Registration(username=username, email=email, password=password)
+        user = Registration(username=username, email=email, password=password, profile_picture=profile_picture)
         user.save()
 
-        messages.success(request, 'Account created successfully')
-        return redirect('login')
+        if user:
+            messages.success(request, 'Account created successfully')
+            request.session['identity'] = email
+            return redirect('home')
+        else:
+            messages.error(request, 'Account creation failed')
+            return redirect('register')
     else:
         return render(request, 'register.html')
 
-
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        try:
+            user = Registration.objects.get(email=email,password=password)
+        except Registration.DoesNotExist:
+            messages.error(request, 'Invalid email or password')
+            return redirect('login')
+
+        if user:
+            messages.success(request, 'Logged in successfully')
+            request.session['identity'] = email
+            return redirect('home')  
+        else:
+            messages.error(request, 'Invalid email or password')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+
 
 def home(request):
     return render(request,'index.html')
