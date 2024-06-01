fc4b7c2ac7c176c1a9122986628220db568ce055
Ahmed Bilal Bhatti <110194946+AhmedBilalSSG@users.noreply.github.com>
2024-06-01 22:06:20 +0500

    commit

--- Grader/urls.py
+++ Grader/urls.py
@@ -3,5 +3,6 @@ from .import views
 from Grader import *
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('', views.register, name='register'),
 ]
--- Grader/views.py
+++ Grader/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
+
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = Registration(username=username, email=email, password=password)
+        user.save()
+
+        messages.success(request, 'Account created successfully')
+        return redirect('login')
+    else:
+        return render(request, 'register.html')
+
+
+def login(request):
+    return render(request,'login.html')
 
 def home(request):
     return render(request,'index.html')
