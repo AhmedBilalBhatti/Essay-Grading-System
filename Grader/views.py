7af66f934851f134d1576cc9d62839eabe4f801e
AhmedBilalSSG <ahmadbilalssg@gmail.com>
2024-06-10 19:17:57 +0500

    commit

--- Grader/views.py
+++ Grader/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse , JsonResponse
 from .models import *
 from django.contrib import messages
 from .nlp import * 
+from .topic import *
 
 
 def register(request):
@@ -61,23 +62,53 @@ def home(request):
     
     return render(request,'index.html', {'user':user})
 
+def slice_string(input_string, length=300):
+    return (input_string[:length] + '...') if len(input_string) > length else input_string
 
 def main(request):
-    email =  request.session.get('identity')
+    email = request.session.get('identity')
     if email:
         user = Registration.objects.get(email=email)
     else:
         return redirect('login')
-
     if request.method == 'POST':
         message = request.POST['message']
-        print(message)
+        new_essay = message
 
-        response = "This is a dummy response."
+        grade, num_errors, num_uncommon_words, num_punctuation, detailed_uncommon_words, avg_sentiment, sentiment_label = grade_and_assess_mistakes(new_essay)
+        # suggestions = improvement_suggestions(new_essay)
+        topic = topic_modelling(new_essay)
+        
+        if sentiment_label == '1 star':
+            sentiment_label = 'Very Negative'
+        elif sentiment_label == '2 stars':
+            sentiment_label = 'Negative'
+        elif sentiment_label == '3 stars':
+            sentiment_label = 'Neutral'
+        elif sentiment_label == '4 stars':
+            sentiment_label = 'Positive'
+        else:
+            sentiment_label = 'Very Positive'
+        
+        detailed_uncommon_words = slice_string(detailed_uncommon_words)
+        print('Ptt================',detailed_uncommon_words)
+        
+        response = {
+            'grade': grade,
+            'num_errors': num_errors,
+            'num_uncommon_words': num_uncommon_words,
+            'num_punctuation': num_punctuation,
+            'detailed_uncommon_words': detailed_uncommon_words,
+            'avg_sentiment': avg_sentiment,
+            'sentiment_label': sentiment_label,
+            # 'suggestions': suggestions,
+            'topic': topic,
+        }
 
         return JsonResponse({'message': response})
 
-    return render(request, 'chatbot.html', {'user':user})
+    return render(request, 'chatbot.html', {'user': user})
+
 
 
 def user_profile(request):
